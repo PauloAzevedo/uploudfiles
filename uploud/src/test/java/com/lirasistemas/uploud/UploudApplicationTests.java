0efbf4df5d4aa5a2bb60fc762f9721b7d60bb225
Paulo Azevedo <paullo.azevedo@gmail.com>
2020-05-11 18:07:45 -0300

    Criação da api para uploud de arquivos em spring boot
    Criação da api para uploud de arquivos em spring boot

---
+++ uploud/src/test/java/com/lirasistemas/uploud/UploudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lirasistemas.uploud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
